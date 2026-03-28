c939c4f5e1b822bb2a50550a40c6c5623b97a776
nivisdeMacBook-Air.local <2670227240@qq.com>
2026-03-28 21:27:17 +0800

    feat: Problem (minimal_ddp_flat_benchmarking): 2 points

--- cs336_systems/ddp_individual_parameters.py
+++ cs336_systems/ddp_individual_parameters.py
@@ -1,7 +1,8 @@
 import torch
 import torch.distributed as dist
 
-
+import torch
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 class DDPIndividualParameters(torch.nn.Module):
     def __init__(self,module: torch.nn.Module):
         super().__init__()
@@ -22,9 +23,16 @@ class DDPIndividualParameters(torch.nn.Module):
     @torch.no_grad()
     def finish_gradient_synchronization(self):
         world_size = dist.get_world_size()
+        grads_buffer=[]
         for param in self.module.parameters():
             if param.grad is None:
                 continue
-            dist.all_reduce(param.grad, op=dist.ReduceOp.SUM)
-            param.grad /= world_size
+            grads_buffer.append(param.grad)
+            # param.grad /= world_size
+        flat = _flatten_dense_tensors(self.reduce_buffer)
+        dist.all_reduce(flat.grad, op=dist.ReduceOp.SUM)
+        flat.div_(world_size)
+        synced_grads = _unflatten_dense_tensors(flat, grads_buffer)
+        for grad, synced_grad in zip(grads_buffer, synced_grads):
+            grad.copy_(synced_grad)
         return 
