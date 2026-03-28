ac1f7fe90f4a8cef14d406bc1aa73ad61aee1776
nivisdeMacBook-Air.local <2670227240@qq.com>
2026-03-28 22:43:37 +0800

    feat: Problem (ddp_overlap_individual_parameters): 5 points

--- cs336_systems/ddp_individual_parameters.py
+++ cs336_systems/ddp_individual_parameters.py
@@ -5,6 +5,8 @@ class DDPIndividualParameters(torch.nn.Module):
     def __init__(self,module: torch.nn.Module):
         super().__init__()
         self.module=module
+        self._post_accumulate_handles=[]
+        self._grad_sync_handles=[]
         with torch.no_grad():
             # for name, param in module.named_parameters():
             #     print(f"[rank {dist.get_rank()}] before broadcast: {name}, device={param.device}, shape={tuple(param.shape)}")
@@ -13,26 +15,33 @@ class DDPIndividualParameters(torch.nn.Module):
             #     torch.cuda.synchronize(param.device)
             #     print(f"[rank {dist.get_rank()}] after broadcast: {name}")
             for param in self.module.parameters():
-                
                 dist.broadcast(param, src=0)
-    def forward(self,x):
-        return self.module(x)
+            for param in self.module.parameters():
+                if not param.requires_grad:
+                    continue
+
+                handle = param.register_post_accumulate_grad_hook(
+                    self._make_post_accumulate_hook()
+                )
+                self._post_accumulate_handles.append(handle)
+
+    def _make_post_accumulate_hook(self):
+        def hook(param: torch.Tensor):
+            if param.grad is None:
+                return
+            work = dist.all_reduce(param.grad, op=dist.ReduceOp.SUM, async_op=True)
+            self._grad_sync_handles.append((param, work))
+
+        return hook
+
+    def forward(self,*input,**kwargs):
+        return self.module(*input,**kwargs)
     
     @torch.no_grad()
     def finish_gradient_synchronization(self):
-        world_size = dist.get_world_size()
-        grads_buffer=[]
-        for param in self.module.parameters():
-            if param.grad is None:
-                continue
-            grads_buffer.append(param.grad)
-            # param.grad /= world_size
-        if not grads_buffer:
-            return
-        flat = _flatten_dense_tensors(grads_buffer)
-        dist.all_reduce(flat, op=dist.ReduceOp.SUM)
-        flat.div_(world_size)
-        synced_grads = _unflatten_dense_tensors(flat, grads_buffer)
-        for grad, synced_grad in zip(grads_buffer, synced_grads):
-            grad.copy_(synced_grad)
+        for param, work in self._grad_sync_handles:
+            work.wait()
+            param.grad /= self.world_size
+
+        self._grad_sync_handles.clear()
         return 
